51823f6992ceb355ca466ebbc6f87a10381dd328
Sam Hutchings <samuel.hutchings@btinternet.com>
2015-10-17 22:35:18 +0100

    Frontend pages frames (Backend todo)

--- graphics-control/server/routes/static.js
+++ graphics-control/server/routes/static.js
@@ -8,6 +8,19 @@ router.get('/', function (req, res, next) {
     res.sendFile(path.resolve(__dirname, '../../client/html/index.html'));
 });
 
+router.get('/settings/', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../../client/html/settings.html'));
+});
+
+router.get('/manual-control/', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../../client/html/manual-control.html'));
+});
+
+router.get('/stats/', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../../client/html/stats.html'));
+});
+
+
 router.use(express.static(path.resolve(__dirname, '../../public/')));
 
 module.exports = router;
