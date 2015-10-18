4e5f379ce936a86342b304661e56556af81f9b04
niklasR <nik.rahmel@bbc.co.uk>
2015-10-18 14:16:44 +0100

    - Open Modal sor subsitution selection

--- graphics-control/client/js/controllers/PlayerControl.js
+++ graphics-control/client/js/controllers/PlayerControl.js
@@ -1,6 +1,7 @@
 angular.module('app')
-    .controller('PlayerControlController', function($scope, TeamSettingsSvc) {
+    .controller('PlayerControlController', function ($scope, $uibModal, TeamSettingsSvc) {
         $scope.data = {};
+        $scope.animationsEnabled = true;
 
         TeamSettingsSvc.updateData();
         TeamSettingsSvc.getUpdates(function (data) {
@@ -13,4 +14,82 @@ angular.module('app')
         };
 
 
+        $scope.open = function (team, size) {
+
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'substituteModal',
+                controller: 'Modal' + team + 'InstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            });
+        };
+
+        $scope.toggleAnimation = function () {
+            $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
+
+
+
+    })
+    .controller('ModalHomeInstanceCtrl', function ($scope, $modalInstance, TeamSettingsSvc) {
+
+        $scope.data = {};
+
+        $scope.selected = {
+            player: ""
+        };
+
+        TeamSettingsSvc.updateData();
+        TeamSettingsSvc.getUpdates(function (data) {
+            $scope.data = data.home.players;
+            $scope.$digest();
+        });
+
+        $scope.setPlayerIsPlaying = function (team, playerNumber, isPlaying) {
+            TeamSettingsSvc.setPlayerIsPlaying(team, playerNumber, isPlaying);
+        };
+
+        $scope.ok = function () {
+            console.log("Selected", "to substitute with")
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+}).controller('ModalGuestInstanceCtrl', function ($scope, $modalInstance, TeamSettingsSvc) {
+
+        $scope.data = {};
+
+        $scope.selected = {
+            player: ""
+        };
+
+        TeamSettingsSvc.updateData();
+        TeamSettingsSvc.getUpdates(function (data) {
+            $scope.data = data.guest.players;
+            $scope.$digest();
+        });
+
+        $scope.setPlayerIsPlaying = function (team, playerNumber, isPlaying) {
+            TeamSettingsSvc.setPlayerIsPlaying(team, playerNumber, isPlaying);
+        };
+
+        $scope.ok = function () {
+            console.log("Selected", "to substitute with", "on team")
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
     });
